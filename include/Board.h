87130ae8b33b089e2b4485b9d3cd0c092079501a
Justin Nappi <jtn8001@gmail.com>
2024-11-27 12:43:35 -0500

    created board header

--- include/Board.h
+++ include/Board.h
@@ -1,1 +1,21 @@
-/* Board header file. */
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+#include "Piece.h"
+
+class Board
+{
+public:
+    Board();
+    void display() const;
+
+private:
+    std::vector<std::vector<std::shared_ptr<Piece>>> grid;
+    void initialize();
+};
+
+#endif
--- include/Piece.h
+++ include/Piece.h
@@ -1,1 +1,17 @@
-/* Piece header file. */
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <string>
+
+class Piece
+{
+public:
+    Piece(const std::string &color, char type) : color(color), type(type) {}
+    char getType() const { return type; }
+
+private:
+    std::string color;
+    char type;
+};
+
+#endif
